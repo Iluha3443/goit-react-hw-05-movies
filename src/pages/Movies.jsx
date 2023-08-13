1210812f985b1d432c3d6693dbbc45ce62561287
Iluha3443 <tkacenkoila01@gmail.com>
2023-08-13 16:30:57 +0300

    task

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useCallback  } from "react";
 import { searchMovies } from '../API';
 import { Link } from "react-router-dom";
 
